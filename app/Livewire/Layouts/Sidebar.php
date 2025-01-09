dfa39f6e1227279a3098c31539712738240af687
reygamarza <marzareyga@gmail.com>
2025-01-09 11:17:34 +0700

    sidebar dinamis

--- app/Livewire/Layouts/Sidebar.php
+++ app/Livewire/Layouts/Sidebar.php
@@ -2,10 +2,20 @@
 
 namespace App\Livewire\Layouts;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Sidebar extends Component
 {
+    public $operator;
+    public $tenagaKependidikan;
+
+    public function mount()
+    {
+        $this->operator = Auth::user()->role == 'operator';
+        $this->tenagaKependidikan = in_array(Auth::user()->role, ['kesiswaan', 'waliKelas']);
+    }
+
     public function render()
     {
         return view('livewire.layouts.sidebar');
--- resources/views/livewire/layouts/sidebar.blade.php
+++ resources/views/livewire/layouts/sidebar.blade.php
@@ -21,30 +21,167 @@
 
         <div class="items-center block w-auto max-h-screen overflow-auto h-sidenav grow basis-full">
             <ul class="flex flex-col pl-0 mb-0">
-                <li class="mt-0.5 w-full">
-                    <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
-                        href="./pages/dashboard.html">
-                        <div
-                            class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                            <i class="relative top-0 text-sm leading-normal text-blue-500 ni ni-tv-2"></i>
-                        </div>
-                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Dashboard</span>
-                    </a>
-                </li>
-                <li class="mt-0.5 w-full">
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button
+                @if ($operator)
+                    <!-- Koordinat | Waktu Presensi -->
+                    <li class="mt-0.5 w-full">
+                        <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
+                            href="./pages/dashboard.html">
+                            <div
+                                class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center relative">
+                                <i class="text-2xl text-blue-500 dark:text-blue-400 ni ni-map-big"></i>
+                            </div>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Koordinat | Waktu
+                                Presensi</span>
+                        </a>
+                    </li>
+
+                    <!-- Kelas | Jurusan -->
+                    <li class="w-full mt-4">
+                        <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">Kelas
+                            |
+                            Jurusan</h6>
+                    </li>
+                    <li class="mt-0.5 w-full">
+                        <a class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
+                            href="./pages/profile.html">
+                            <div
+                                class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                <i class="text-green-500 dark:text-green-400 text-lg ni ni-building"></i>
+                            </div>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Daftar Kelas</span>
+                        </a>
+                    </li>
+                    <li class="mt-0.5 w-full">
+                        <a class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
+                            href="./pages/profile.html">
+                            <div
+                                class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                <i class="text-yellow-500 dark:text-yellow-400 text-lg ni ni-hat-3"></i>
+                            </div>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Daftar Jurusan</span>
+                        </a>
+                    </li>
+
+                    <!-- Daftar Pengguna -->
+                    <li class="w-full mt-4">
+                        <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">
+                            Daftar
+                            Pengguna</h6>
+                    </li>
+                    <li class="mt-0.5 w-full">
+                        <a class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
+                            href="./pages/profile.html">
+                            <div
+                                class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                <i class="text-red-500 dark:text-red-400 text-lg ni ni-circle-08"></i>
+                            </div>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Kesiswaan</span>
+                        </a>
+                    </li>
+                    <li class="mt-0.5 w-full">
+                        <a class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
+                            href="./pages/profile.html">
+                            <div
+                                class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                <i class="text-indigo-500 dark:text-indigo-400 text-lg ni ni-briefcase-24"></i>
+                            </div>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Wali Kelas</span>
+                        </a>
+                    </li>
+                    <li class="mt-0.5 w-full">
+                        <a class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
+                            href="./pages/profile.html">
+                            <div
+                                class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                <i class="text-purple-500 dark:text-purple-400 text-lg ni ni-user-run"></i>
+                            </div>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Wali Siswa</span>
+                        </a>
+                    </li>
+
+                    <!-- Settings -->
+                    <li class="w-full mt-4">
+                        <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">
+                            Settings</h6>
+                    </li>
+                    <li class="mt-0.5 w-full">
+                        <a
                             class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors">
                             <div
                                 class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                                <i
-                                    class="relative top-0 text-sm leading-normal text-orange-500 ni ni-single-copy-04"></i>
+                                <i class="text-blue-500 dark:text-blue-400 text-lg ni ni-badge"></i>
                             </div>
-                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Logout</span>
-                        </button>
-                    </form>
-                </li>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Profile</span>
+                        </a>
+                    </li>
+                    <li class="mt-0.5 w-full">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button
+                                class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors">
+                                <div
+                                    class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                    <i class="text-orange-500 dark:text-orange-400 text-lg ni ni-button-power"></i>
+                                </div>
+                                <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Logout</span>
+                            </button>
+                        </form>
+                    </li>
+                @else
+                    <!-- Dashboard -->
+                    <li class="mt-0.5 w-full">
+                        <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
+                            href="./pages/dashboard.html">
+                            <div
+                                class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center relative">
+                                <i class="text-2xl text-blue-500 ni ni-tv-2"></i>
+                            </div>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Dashboard</span>
+                        </a>
+                    </li>
+
+                    <!-- Laporan Absensi -->
+                    <li class="mt-0.5 w-full">
+                        <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
+                            href="./pages/dashboard.html">
+                            <div
+                                class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center relative">
+                                <i class="text-2xl text-blue-500 ni ni-books"></i>
+                            </div>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Laporan Absensi</span>
+                        </a>
+                    </li>
+
+                    <!-- Settings -->
+                    <li class="w-full mt-4">
+                        <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">
+                            Settings</h6>
+                    </li>
+                    <li class="mt-0.5 w-full">
+                        <a
+                            class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors">
+                            <div
+                                class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                <i class="text-blue-500 text-lg ni ni-badge"></i>
+                            </div>
+                            <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Profile</span>
+                        </a>
+                    </li>
+                    <li class="mt-0.5 w-full">
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+                            <button
+                                class="dark:text-white dark:opacity-80 py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors">
+                                <div
+                                    class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                                    <i class="text-orange-500 text-lg ni ni-button-power"></i>
+                                </div>
+                                <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Logout</span>
+                            </button>
+                        </form>
+                    </li>
+                @endif
+
             </ul>
         </div>
     </aside>
